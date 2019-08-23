f87ce98a49684d56cbfa12ce0dad47c57392e807
Rafael Guerra <rafaelsguerra@hotmail.com>
2019-08-23 03:08:21 -0300

    adding component communication and databinding to course project

--- course-project/prj-start/src/app/recipes/recipe-list/recipe-list.component.ts
+++ course-project/prj-start/src/app/recipes/recipe-list/recipe-list.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, EventEmitter, Output } from '@angular/core';
 
 import { Recipe } from '../recipe.model';
 
@@ -8,13 +8,20 @@ import { Recipe } from '../recipe.model';
   styleUrls: ['./recipe-list.component.css']
 })
 export class RecipeListComponent implements OnInit {
+  @Output() recipeWasSelected = new EventEmitter<Recipe>();
+  
   recipes: Recipe[] = [
-    new Recipe("A test recipe", "This is a test", "https://cdn.pixabay.com/photo/2016/06/15/19/09/food-1459693_960_720.jpg")
+    new Recipe("A test recipe", "This is a test", "https://cdn.pixabay.com/photo/2016/06/15/19/09/food-1459693_960_720.jpg"),
+    new Recipe("Another recipe", "Another test", "https://cdn.pixabay.com/photo/2015/08/18/22/56/fruit-895005_960_720.png")
   ];
 
   constructor() { }
 
   ngOnInit() {
+  }
+
+  onRecipeSelected(recipe: Recipe) {
+    this.recipeWasSelected.emit(recipe);
   }
 
 }
