c08a8a2eb4a137fcea74b3153a7a851377ca45a5
Rafael Guerra <rafaelsguerra@hotmail.com>
2019-12-04 01:13:31 -0300

    adding routing to course project

--- course-project/prj-start/src/app/recipes/recipe-list/recipe-item/recipe-item.component.ts
+++ course-project/prj-start/src/app/recipes/recipe-list/recipe-item/recipe-item.component.ts
@@ -1,7 +1,6 @@
 import { Component, OnInit, Input } from '@angular/core';
 
 import { Recipe } from '../../recipe.model';
-import { RecipeService } from '../../recipes.service';
 
 @Component({
   selector: 'app-recipe-item',
@@ -11,13 +10,9 @@ import { RecipeService } from '../../recipes.service';
 export class RecipeItemComponent implements OnInit {
   @Input() recipe: Recipe;
 
-  constructor(private recipeService: RecipeService) { }
+  @Input() index: number;
 
   ngOnInit() {
-  }
-
-  onClickRecipe() {
-    this.recipeService.recipeSelected.emit(this.recipe);
   }
 
 }
--- course-project/prj-start/src/app/recipes/recipe-list/recipe-list.component.ts
+++ course-project/prj-start/src/app/recipes/recipe-list/recipe-list.component.ts
@@ -2,6 +2,7 @@ import { Component, OnInit } from '@angular/core';
 
 import { Recipe } from '../recipe.model';
 import { RecipeService } from '../recipes.service';
+import { Router, ActivatedRoute } from '@angular/router';
 
 @Component({
   selector: 'app-recipe-list',
@@ -11,7 +12,11 @@ import { RecipeService } from '../recipes.service';
 export class RecipeListComponent implements OnInit {
   recipes: Recipe[] = [];
 
-  constructor(private recipeService: RecipeService) { }
+  constructor(private recipeService: RecipeService, private router: Router, private route: ActivatedRoute) { }
+
+  onNewRecipe() {
+    this.router.navigate(['new'], {relativeTo: this.route})
+  }
 
   ngOnInit() {
     this.recipes = this.recipeService.getRecipes();
