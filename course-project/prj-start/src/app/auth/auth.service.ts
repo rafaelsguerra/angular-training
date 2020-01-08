56bd2cf2be8ce30f6fbe334f5a63569192b11f58
Rafael Guerra <rafaelsguerra@hotmail.com>
2020-01-07 23:19:45 -0300

    adding authentication to course project

---
+++ course-project/prj-start/src/app/auth/auth.service.ts
@@ -0,0 +1,124 @@
+import { Injectable } from '@angular/core';
+import { HttpClient, HttpErrorResponse } from '@angular/common/http';
+import { Router } from '@angular/router';
+
+import { environment } from '../../environments/environment'
+import { catchError, tap } from 'rxjs/operators';
+import { throwError, Subject, BehaviorSubject } from 'rxjs';
+import { User } from './user.model';
+
+export interface AuthResponseData {
+  kind: string;
+  idToken: string;
+  email: string;
+  refreshToken: string;
+  expiresIn: string;
+  localId: string;
+  registered?: boolean;
+}
+
+@Injectable({ providedIn: "root" })
+export class AuthService {
+
+  constructor(private http: HttpClient, private router: Router) { }
+
+  apiKey = environment.apiKey;
+  user = new BehaviorSubject<User>(null);
+  private tokenExpirationTimer: any;
+
+  signup(email: string, password: string) {
+    return this.http.post<AuthResponseData>(
+      // https://identitytoolkit.googleapis.com/v1/accounts:signUp?key=AIzaSyDWG5CkH_mCz-jTFNi1Wd9rLqltSLNo9Rk
+      'https://identitytoolkit.googleapis.com/v1/accounts:signUp?key=' + this.apiKey,
+      {
+        email: email,
+        password: password,
+        returnSecureToken: true
+      }
+    ).pipe(catchError(this.handleError), tap(responseData => {
+      const expirationDate = new Date(new Date().getTime() + +responseData.expiresIn * 1000);
+      this.handleAuthentication(responseData.email, responseData.localId, responseData.idToken, +responseData.expiresIn);
+    }));
+  }
+
+  login(email: string, password: string) {
+    return this.http.post<AuthResponseData>(
+      'https://identitytoolkit.googleapis.com/v1/accounts:signInWithPassword?key=' + this.apiKey,
+      {
+        email: email,
+        password: password,
+        returnSecureToken: true
+      }
+    ).pipe(catchError(this.handleError), tap(responseData => {
+      this.handleAuthentication(responseData.email, responseData.localId, responseData.idToken, +responseData.expiresIn)
+    }));
+  }
+
+  logout() {
+    this.user.next(null);
+    this.router.navigate(['/auth']);
+    localStorage.removeItem('userData');
+
+    if (this.tokenExpirationTimer) {
+      clearTimeout(this.tokenExpirationTimer);
+    }
+    this.tokenExpirationTimer = null;
+  }
+
+  autoLogin() {
+    const userData: {
+      email: string, 
+      id: string, 
+      _token: string, 
+      _tokenExpirtionDate: string } = JSON.parse(localStorage.getItem('userData'));
+
+    if (!userData) {
+      return;
+    }
+
+    const loadedUser = new User(userData.email, userData.id, userData._token, new Date(userData._tokenExpirtionDate));
+
+    if (loadedUser.token) {
+      this.user.next(loadedUser);
+      const expirationDuration = new Date(userData._tokenExpirtionDate).getTime() - new Date().getTime();
+      this.autoLogout(expirationDuration);
+    }
+  }
+
+  autoLogout(expirationDuration: number) {
+    this.tokenExpirationTimer = setTimeout(() => {
+      window.alert('Session expired! You have been logged out.'); 
+      this.logout();
+     }, expirationDuration);
+  }
+
+  private handleAuthentication(email: string, userId: string, token: string, expiresIn: number) {
+    const expirationDate = new Date(new Date().getTime() + expiresIn * 1000);
+      const user = new User(email, userId, token, expirationDate);
+      this.user.next(user);
+      this.autoLogout(expiresIn * 1000);
+      localStorage.setItem('userData', JSON.stringify(user))
+  }
+
+  private handleError(errorResponse: HttpErrorResponse) {
+    let errorMessage = "An unknown error ocurred!";
+
+    if (!errorResponse.error || !errorResponse.error.error) {
+      return throwError(errorMessage);
+    }
+
+    switch (errorResponse.error.error.message) {
+      case 'EMAIL_EXISTS':
+        errorMessage = "This email already exists.";
+        break;
+      case 'EMAIL_NOT_FOUND':
+        errorMessage = "This email does not exist.";
+        break;
+      case 'INVALID_PASSWORD':
+        errorMessage = "This password is incorrect.";
+        break;
+    }
+
+    return throwError(errorMessage);
+  }
+}
--- course-project/prj-start/src/app/shared/data-storage.service.ts
+++ course-project/prj-start/src/app/shared/data-storage.service.ts
@@ -1,16 +1,19 @@
 import { Injectable } from '@angular/core';
-import { HttpClient } from '@angular/common/http';
+import { HttpClient, HttpParams } from '@angular/common/http';
 
+import { environment } from '../../environments/environment'
 import { RecipeService } from '../recipes/recipes.service';
 import { Recipe } from '../recipes/recipe.model';
-import { map, tap } from 'rxjs/operators';
+import { map, tap, take, exhaustMap } from 'rxjs/operators';
+import { AuthService } from '../auth/auth.service';
 
 @Injectable({ providedIn: 'root' })
 export class DataStorageService {
 
-  constructor(private http: HttpClient, private recipeService: RecipeService) { }
+  constructor(private http: HttpClient, private recipeService: RecipeService, private authService: AuthService) { }
 
-  recipesUrl = ''; // This url won't be shared to avoid undesirable requests to the database
+  // This url won't be shared to avoid undesirable requests to the database
+  recipesUrl = environment.apiUrl;
 
   storeRecipes() {
     const recipes = this.recipeService.getRecipes();
@@ -25,7 +28,7 @@ export class DataStorageService {
         return recipes.map(recipe => {
           return { ...recipe, ingredients: recipe.ingredients ? recipe.ingredients : [] }
         });
-      }), 
+      }),
       tap(recipes => {
         this.recipeService.setRecipes(recipes);
       })
